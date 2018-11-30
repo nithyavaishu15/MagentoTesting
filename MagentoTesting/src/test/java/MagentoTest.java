e13cb155eee7a789d298fca0aa26e09bffd7dedf
Sucheendra <Sucheendra@192.168.1.175>
2018-11-30 16:05:15 +0530

    Chrome Driver included and email is changed

--- MagentoTesting/src/test/java/MagentoTest.java
+++ MagentoTesting/src/test/java/MagentoTest.java
@@ -2,24 +2,22 @@
 
 import java.util.concurrent.TimeUnit;
 
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 
 public class MagentoTest {
 
 	@Test
-	public void positiveCredential1() {
-		
-	} {
-		FirefoxDriver driver = new FirefoxDriver();
+	public void positiveCredential1()  {
+		ChromeDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 		driver.get("https://www.magento.com");
 		Welcome w = new Welcome(driver);
 		w.clickOnMyAcct();
 		Login l = new Login(driver);
-		l.typeEmail("nitinmanjunath1991@gmail.com");
+		l.typeEmail("sucheendra.abc@gmail.com");
 		l.typePass("Welcome123");
 		l.clickOnLogin();
 		Main m = new Main(driver);
