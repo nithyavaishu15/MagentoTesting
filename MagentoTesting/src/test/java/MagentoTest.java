d8e97f3c6a9a7b87c1b6e6e18cfb127d14976847
Sucheendra <Sucheendra@192.168.1.175>
2018-11-30 15:59:33 +0530

    First commit

---
+++ MagentoTesting/src/test/java/MagentoTest.java
@@ -0,0 +1,31 @@
+
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+
+public class MagentoTest {
+
+	@Test
+	public void positiveCredential1() {
+		
+	} {
+		FirefoxDriver driver = new FirefoxDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		driver.get("https://www.magento.com");
+		Welcome w = new Welcome(driver);
+		w.clickOnMyAcct();
+		Login l = new Login(driver);
+		l.typeEmail("nitinmanjunath1991@gmail.com");
+		l.typePass("Welcome123");
+		l.clickOnLogin();
+		Main m = new Main(driver);
+		m.clickOnLogout();
+		driver.quit();
+
+	}
+
+}
